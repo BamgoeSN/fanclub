18961d5e03d2f5a37c48cdcfcaa456cad722042d
BamgoeSN <60645387+BamgoeSN@users.noreply.github.com>
2024-09-10 17:25:19 +0900

    Complete without designs

---
+++ script.js
@@ -0,0 +1,44 @@
+function tokenizeText(text) {
+  return text
+    .split("\n")
+    .map((s) => s.trim().split(/\s+/))
+    .filter((arr) => arr.length === 2);
+}
+
+function generateCode(setter, tokens) {
+  const header =
+    "# Author: " + setter + "\n\nimport random as r\n\nparticipants = (\n    ";
+  const tuple = tokens
+    .map((t) => "['" + t[0] + "'] * " + t[1])
+    .join("\n    + ");
+  const footer = "\n)\n\nprint(r.choice(participants))";
+  return header + tuple + footer;
+}
+
+function setCode() {
+  const setter = document.getElementsByName("setter")[0].value;
+  const tokens = tokenizeText(
+    document.getElementsByName("list-input")[0].value,
+  );
+  const code = generateCode(setter, tokens);
+  document.getElementsByName("code-text")[0].textContent = code;
+}
+
+function wrapWithCodeBlock(code) {
+  return "./run py\n```py\n" + code + "\n```";
+}
+
+function setCopyAlert(text) {
+  document.getElementsByName("copy-alert")[0].textContent = text;
+}
+
+function copyCodeToClipboard() {
+  const code = document.getElementsByName("code-text")[0].textContent;
+  const data = wrapWithCodeBlock(code);
+  window.navigator.clipboard.writeText(data).then(() => {
+    setCopyAlert("Copied!");
+    setTimeout(function () {
+      setCopyAlert("");
+    }, 2000);
+  });
+}
